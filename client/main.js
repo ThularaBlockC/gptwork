4c93c52020cb9c46a6e362a63845c0cef5dc22be
ThularaBlockC <thularablockchain@gmail.com>
2022-12-29 23:27:15 +1100

    update the backend link

--- client/main.js
+++ client/main.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   const messageView = document.getElementById(uniqueId)
   loaderAnim(messageView)
 
-  const response = await fetch('http://localhost:3000',{
+  const response = await fetch('https://gptwork.onrender.com',{
     method: 'Post',
     headers: {
       'content-Type': 'application/json'
